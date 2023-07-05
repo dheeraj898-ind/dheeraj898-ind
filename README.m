a16a98580e062a742eaefc4593b518c6504fb789
dheeraj898-ind <134209019+dheeraj898-ind@users.noreply.github.com>
2023-07-05 10:48:46 +0530

    My self
    I am fresher and I will paas out in 2024

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi I’m @dheeraj898-ind
+- 👀 I’m interested in ...vlsi domain job.
+- 🌱 I’m currently learning ...vlsi,java,sql.
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ... dheerajbasi@gmail.com,8985089868.
+
+<!---
+dheeraj898-ind/dheeraj898-ind is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
